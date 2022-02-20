afd01150f6345e4c157e75d2b15f2c67bcea16ee
Akash Saxena <akashsaxena5555@gmail.com>
2022-02-21 02:58:17 +0530

    add additional requested functionality

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # calculator
 
 Current Problems:
-1. At .display > .input, text is overflowing outside the display zone. We want the long inputs to wrap below the first string of input. 
+1. At .display > .input, text is overflowing outside the display zone. We want the long inputs to wrap below the first string of input.
+
+Learnings:
+1. The variables declared without the var keyword becomes global variables, irrespective of where they are declared. [use 'use strict'; inside the function to avoid this behaviour if the variable is being used without its declarator.] [http://blog.niftysnippets.org/2008/03/horror-of-implicit-globals.html] [https://stackoverflow.com/questions/6888570/declaring-variables-without-var-keyword]
--- script/script.js
+++ script/script.js
@@ -1,28 +1,95 @@
-function add(){
+let operatorFlag = false;
+let number1 = '';
+let number2 = '';
+let screen = document.querySelector(".display > .input");
+let answer = document.querySelector(".display > .answer");
+let operatorCount = 0;
+let result = 0; //program is running fine if i remove result/operatorValue declararion... undeclared variable names used inside a function defaults to global behaviour
+let operatorValue = '';
 
+function add(){
+    result = parseFloat(number1) + parseFloat(number2);
 }
 function subtract(){
-
+    result = parseFloat(number1) - parseFloat(number2);
 }
 function multiply(){
-
+    result = parseFloat(number1) * parseFloat(number2);
 }
 function divide(){
-
+    result = parseFloat(number1) / parseFloat(number2);
 }
-function operate(number1,number2,operation){
 
+
+function operate(operatorValue){
+    if (operatorValue == '+')
+    {
+        add();
+    }
+    else if(operatorValue == '-')
+    {
+        subtract();
+    }
+    else if(operatorValue == '*')
+    {
+        multiply();
+    }
+    else if(operatorValue == '/')
+    {
+        divide();
+    }
+    answer.textContent = result;
+    number1 = result.toString();  //number1 is a int here
 }
-let decimals = document.querySelectorAll(".decimal-buttons > button");
-let number1 = '';
-let screen = document.querySelector(".display > .input");
-// decimals.forEach(key => key.addEventListener('click',(e) => {
-//     number1 = number1.concat(e.target.dataset.key);
-//     console.log(number1);
-// }));
+
+
+function setOperator(value){
+    operatorFlag = true;
+    document.getElementById("dot").disabled = false; 
+    operatorCount += 1;
+    if (operatorCount>1)
+    {
+        equals();
+    }
+    operatorValue = value;
+    screen.textContent += value;
+}
 function setValue(value){
-    number1 = number1.concat(value);
-    screen.textContent = number1;
+    if (value == '.')
+    {
+        document.getElementById("dot").disabled = true; 
+    }
+    if (operatorFlag)
+    {
+        number2 = number2.concat(value);
+    }
+    else
+    {
+        number1 = number1.concat(value);
+    }
+    screen.textContent += value;
 }
 
 
+function equals(){
+    if (number2 == '0' && operatorValue == '/')
+    {
+        alert('This corner case is yet to be solved by the Mathematicians.')
+        clears();
+        return 0;
+    }
+    operate(operatorValue);
+    number2 = '';
+    operatorCount -= 1;
+    screen.textContent = '';
+}
+function clears(){
+    operatorFlag = false;
+    operatorCount = 0;
+    result = 0;
+    number1 = '';
+    number2 = '';
+    answer.textContent = '';
+    screen.textContent = '';
+    operatorValue = '';
+}
